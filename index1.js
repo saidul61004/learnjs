acdfa96b37af5df907dd446882bc84ab90e70288
saidul61004 <saidulkhan1511@gmail.com>
2026-05-22 12:26:33 -0400

    first commit

---
+++ index1.js
@@ -0,0 +1,52 @@
+// insert mode  i
+
+// variable declaration
+
+// three type of variable
+
+// const  - immutable value can't assign again hoisting not allowed
+// let - mutable value can assign again cant use variable without intitalization hoisting not allowed
+// var - mutable hoisting supported
+
+// const a = 3
+// a =2 
+// console.log(a) // ->  assignment error
+
+// let a = 3
+// a =2 
+// console.log(a) // ->  no assignment error
+
+
+// hoisting use variable before initialization
+
+
+// console.log(a) // -> not supported hoisting got error 
+// let a = 3
+// a =2 
+
+
+// console.log(a) // ->  supported hoisting no error 
+// var a = 3
+// a =2 
+
+
+// if else conditon
+
+age = 12
+
+if (age>12){
+    console.log('bada hogya hai')
+}
+else{
+
+    console.log('chota hai')
+}
+
+
+
+
+
+
+
+
+
